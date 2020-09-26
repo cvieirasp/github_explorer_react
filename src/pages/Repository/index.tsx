85171ca310f89a7820e365064430295264597728
Carlos Figueiredo <cvieirasp@gmail.com>
2020-09-26 15:52:25 -0300

    Página Detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,102 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+import logoImage from '../../assets/logo.svg';
+import { Header, RepoInfo, IssuesInfo } from './styles';
 
 interface RepoParams {
   repo: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepoParams>();
-  return <h1>Repositório: {params.repo}</h1>;
+  const [repo, setRepo] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repo}`).then((response) => {
+      setRepo(response.data);
+    });
+
+    api.get(`repos/${params.repo}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params.repo]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImage} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      {repo && (
+        <RepoInfo>
+          <header>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repo.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repo.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repo.open_issues_count}</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </RepoInfo>
+      )}
+
+      <IssuesInfo>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url} target="_blank">
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </IssuesInfo>
+    </>
+  );
 };
 
 export default Repository;
